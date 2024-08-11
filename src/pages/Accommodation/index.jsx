a36cd8f9116ca89a53a5c6f3c7284f2330ecdd4e
fatima assila <fatima.assila.ouchene@gmail.com>
2024-08-11 15:58:16 +0200

    add components pages @ routing

---
+++ src/pages/Accommodation/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Accommodation () {
+    return(
+        <h1>loggement </h1>
+    )   
+}
+export default Accommodation
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+// import imgBanner from '../../assets/home-cover.png'
+
+// function Home() {
+//     return (
+//       <div>
+//         <h1> Chez vous, partout et ailleurs</h1>
+//         <img src={imgBanner} alt='image de couverture' />
+//       </div>
+//     )
+//   }
+  
+//   export default Home
+import React from 'react'
+
+function Home() {
+    return (
+        <h1>Welcome </h1>
+    )
+}
+
+export default Home
